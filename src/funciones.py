42f8c23fedc4f59f509abd4287f041117dfde670
carlos <hanx1102@gmail.com>
2026-03-11 13:18:39 -0500

    punto 11 : mejora de la funcion , ahora con redondeo

--- src/funciones.py
+++ src/funciones.py
@@ -1,4 +1,11 @@
 def promedio_notas(notas):
     if not notas:   
         return 0
-    return sum(notas)/len(notas)
+
+    notas_validas = [n for n in notas if 0 <= n <= 5]
+ 
+    if not notas_validas:
+        return 0
+ 
+    resultado = sum(notas_validas) / len(notas_validas)
+    return round(resultado, 2) 
